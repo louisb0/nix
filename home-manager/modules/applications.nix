337ac7207e206112de4464e0bb1a3d06ca3287a5
louisb0 <louisbaragwanath@outlook.com>
2025-06-12 16:41:33 +1200

    Configure OBS properly

--- home-manager/modules/applications.nix
+++ home-manager/modules/applications.nix
@@ -1,5 +1,12 @@
 {pkgs, ...}: {
-  programs.obs-studio.enable = true;
+  programs.obs-studio = {
+    enable = true;
+    plugins = with pkgs.obs-studio-plugins; [
+      wlrobs
+      obs-backgroundremoval
+      obs-pipewire-audio-capture
+    ];
+  };
 
   home.packages = with pkgs; [
     brave
