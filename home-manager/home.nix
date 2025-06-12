52681c49a2c53cca56644644ddb837db8356ba33
louisb0 <louisbaragwanath@outlook.com>
2025-06-12 19:55:29 +1200

    Refactor recording programs

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,10 +1,13 @@
-{inputs, ...}: {
+{
+  pkgs,
+  inputs,
+  ...
+}: {
   imports = [
     inputs.nixvim.homeManagerModules.nixvim
 
     ./modules/theme.nix
-
-    ./modules/applications.nix
+    ./modules/recording.nix
 
     ./modules/graphical/desktop.nix
     ./modules/graphical/kanshi.nix
@@ -22,6 +25,27 @@
     sessionVariables = {
       EDITOR = "nvim";
     };
+
+    packages = with pkgs; [
+      mpv
+      brave
+      anki
+
+      # TODO: Temporary.
+      pandoc
+      texlive.combined.scheme-medium
+      (rstudioWrapper.override {
+        packages = with rPackages; [
+          rmarkdown
+          urca
+          knitr
+          tidyverse
+          fpp3
+          kableExtra
+          bookdown
+        ];
+      })
+    ];
 
     stateVersion = "25.05";
   };
---
+++ home-manager/modules/recording.nix
@@ -0,0 +1,18 @@
+{pkgs, ...}: {
+  xdg.portal = {
+    enable = true;
+    wlr.enable = true;
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-wlr
+      xdg-desktop-portal-gtk
+    ];
+    config.common.default = "*";
+  };
+
+  programs.obs-studio = {
+    enable = true;
+    plugins = with pkgs.obs-studio-plugins; [
+      obs-pipewire-audio-capture
+    ];
+  };
+}
