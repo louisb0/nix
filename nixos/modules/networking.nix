ada2cbd5211d14600d70b8c6a1699cc75a12d783
louisb0 <louisbaragwanath@outlook.com>
2025-08-03 16:53:07 +1200

    Add discord to blocked sites

--- nixos/modules/networking.nix
+++ nixos/modules/networking.nix
@@ -7,6 +7,17 @@
       127.0.0.1 www.reddit.com
       127.0.0.1 linkedin.com
       127.0.0.1 www.linkedin.com
+      127.0.0.1 discord.com
+      127.0.0.1 www.discord.com
+      127.0.0.1 discord.app
+      127.0.0.1 www.discord.app
+      127.0.0.1 discord.gg
+      127.0.0.1 www.discord.gg
+      127.0.0.1 gateway.discord.gg
+      127.0.0.1 cdn.discordapp.com
+      127.0.0.1 media.discordapp.net
+      127.0.0.1 images-ext-1.discordapp.net
+      127.0.0.1 images-ext-2.discordapp.net
     '';
   };
 }
